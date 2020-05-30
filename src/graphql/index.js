491659ae341524a4fef8b9023de50d06c470ae5c
joao tavares <joao.arthur.tavares@gmail.com>
2020-05-30 15:08:41 -0300

    refac(Campaign): change monsterType to CampaignType

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,16 +1,14 @@
 import { makeExecutableSchema } from 'graphql-tools'
-import {gql} from 'apollo-server-koa'
 
 // Scheme Content
-import typeDefs from './type-defs'
+// Import typeDefs from './type-defs'
 import resolvers from './resolvers'
 
-import monsterType from './monsters'
 import campaingType from './campaigns'
 
 const executableSchema = makeExecutableSchema({
-	// typeDefs,
-	typeDefs: monsterType,
+	// TypeDefs,
+	typeDefs: campaingType,
 	resolvers,
 })
 
